153e43a5b5d1e1c264441de1c72d7902413a952e
BhavyaT-135 <bhavyatewari.13.5@gmail.com>
2022-07-04 09:19:10 +0530

    ⚡️ Creating a route to send json object

--- index.js
+++ index.js
@@ -3,7 +3,28 @@ const path = require('path');
 
 const app = express();
 
-//Set sraric folder
+const members = [{
+        id: 1,
+        name: 'John Doe',
+        status: 'active'
+    },
+    {
+        id: 2,
+        name: 'Bhavya Tewari',
+        status: 'inactive'
+    },
+    {
+        id: 3,
+        name: 'Bhavya Verma',
+        status: 'active'
+    }
+]
+
+app.get('/api/members', (req, res) => {
+    res.json(members);
+ })
+
+//Set static folder
 app.use(express.static(path.join(__dirname, 'public')));
 
 const PORT = process.env.PORT || 5000;
