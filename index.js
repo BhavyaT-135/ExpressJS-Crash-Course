6cdb4ac94aa5ab68a6c2f2fedce50eba0abf5af6
BhavyaT-135 <bhavyatewari.13.5@gmail.com>
2022-07-04 09:07:34 +0530

    ⚡️ SendFile response to load html file

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const express = require('express');
+const path = require('path');
 
 const app = express();
 
 //Route for the index page
 //With every route, we will have access to the req and res objects
 app.get('/', function(req,res){
-    res.send('<h1>Hello World</h1>');
+    res.sendFile(path.join(__dirname, 'public', '/index.html'));
 });
 
 const PORT = process.env.PORT || 5000;
