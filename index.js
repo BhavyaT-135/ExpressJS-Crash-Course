bd042fbcc0bd61d1dc2cac12e8ed8b80ab8325a0
BhavyaT-135 <bhavyatewari.13.5@gmail.com>
2022-07-06 17:25:55 +0530

    ⚡️ Created a view using express handlebars

--- index.js
+++ index.js
@@ -9,12 +9,15 @@ const app = express();
 // app.use(logger);
 
 //Handlebars Middleware
-app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.engine('handlebars', exphbs.engine({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
 
 //Body Parser Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
+//Homepage Route
+app.get('/', (req,res) => res.render('index'))
 
 //Set static folder
 app.use(express.static(path.join(__dirname, 'public')));
