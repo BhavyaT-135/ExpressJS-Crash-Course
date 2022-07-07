bc416e2b7f6f3259e1ce8fb8a83eb5f4296bc02f
BhavyaT-135 <bhavyatewari.13.5@gmail.com>
2022-07-08 02:05:57 +0530

    ⚡️ Adding emails to Members.js

--- Members.js
+++ Members.js
@@ -1,16 +1,19 @@
 const members = [{
     id: 1,
     name: 'John Doe',
+    email: 'johndoe@gmail.com',
     status: 'active'
 },
 {
     id: 2,
     name: 'Bhavya Tewari',
+    email: 'bhavyatewari.13.5@gmail.com',
     status: 'inactive'
 },
 {
     id: 3,
     name: 'Bhavya Verma',
+    email:'vbhavya269@gmail.com',
     status: 'active'
 }
 ];
